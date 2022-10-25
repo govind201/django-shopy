ec1c076a0509f2fffe24cc59eae656d864d8d81a
Govind Singh <gs5818475@gmail.com>
2022-10-26 00:46:52 +0530

    push to heroku

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -7,10 +7,10 @@ SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront3',
+        'NAME': 'shopy',
         'HOST': 'mysql',
         'USER': 'root',
-        'PASSWORD': 'MyPassword'
+        'PASSWORD': 'govind'
     }
 }
 
