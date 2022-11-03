e96b633ff7b5ccedbcc78fd6b3297a201bc5c0f4
Govind Singh <gs5818475@gmail.com>
2022-11-03 23:16:53 +0530

    deploy app

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -6,26 +6,26 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['shopy-prod.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': dj_database_url.config()
 }
 
-# REDIS_URL = os.environ['REDIS_URL']
+REDIS_URL = os.environ['REDIS_URL']
 
-# CELERY_BROKER_URL = REDIS_URL
+CELERY_BROKER_URL = REDIS_URL
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': REDIS_URL,
-#         'TIMEOUT': 10 * 60,
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#         }
-#     }
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': REDIS_URL,
+        'TIMEOUT': 10 * 60,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
 # EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
